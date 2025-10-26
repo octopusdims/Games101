534a17b725441785ae2138fc80fb29299f438086
OCTOPUSdims <OCTOPUSdims@gmail.com>
2025-10-26 14:00:00 +0800

    PA0: Finish assignment

--- pa0/main.cpp
+++ pa0/main.cpp
@@ -3,6 +3,13 @@
 #include<eigen3/Eigen/Dense>
 #include<iostream>
 
+Eigen::Vector2f Transformation(Eigen::Vector2f Point, Eigen::Matrix3f Rotation, Eigen::Matrix3f Bias){
+    Eigen::Vector3f t(Point(0), Point(1), 1);
+    t = Bias * Rotation * t;
+    Eigen::Vector2f ret(t(0),t(1));
+    return ret;
+}
+
 int main(){
 
     // Basic Example of cpp
@@ -29,6 +36,9 @@ int main(){
     std::cout << "Example of scalar multiply \n";
     std::cout << v * 3.0f << std::endl;
     std::cout << 2.0f * v << std::endl;
+    // vector dot product
+    std::cout << "Example of Dot Product \n";
+    std::cout << v.dot(w) << std::endl;
 
     // Example of matrix
     std::cout << "Example of matrix \n";
@@ -40,9 +50,24 @@ int main(){
     std::cout << "Example of output \n";
     std::cout << i << std::endl;
     // matrix add i + j
+    std::cout << "Example of matrix add \n";
+    std::cout << i + j << std::endl;
     // matrix scalar multiply i * 2.0
+    std::cout << "Example of matrix scalar multiply \n";
+    std::cout << i * 2.0f << std::endl;
     // matrix multiply i * j
+    std::cout << "Example of matrix multiply \n";
+    std::cout << i * j << std::endl;
     // matrix multiply vector i * v
-
+    std::cout << "Example of matrix multiply vector \n";
+    std::cout << i * v << std::endl;
+    //Transformation
+    Eigen::Matrix3f Rotation,Bias;
+    Rotation << std::cos(M_PI/4), -std::sin(M_PI/4), 0, std::sin(M_PI/4), std::cos(M_PI/4), 0, 0, 0, 1;
+    Bias << 1.0, 0.0, 1.0, 0.0, 1.0, 2.0, 0.0, 0.0, 1.0;
+    Eigen::Vector2f Point(2.0f, 1.0f);
+    std::cout<<"Point:\n"<<Point<<std::endl;
+    Eigen::Vector2f Result = Transformation(Point, Rotation, Bias);
+    std::cout<<"Result:\n"<<Result<<std::endl;
     return 0;
 }
